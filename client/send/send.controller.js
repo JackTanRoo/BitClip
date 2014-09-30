40f50a399eefcddf750c3b74ac8fd2cfad888ed1
JackTanRoo <ordo_sagittae@hotmail.com>
2014-09-29 23:16:05 -0700

    Check point - tests for Utilities in progress

--- client/send/send.controller.js
+++ client/send/send.controller.js
@@ -1,6 +1,6 @@
 angular.module('bitclip.sendController', [
   'ngFx'
-  ])
+])
 
 .controller('sendController', ['$scope', '$timeout', 'TxBuilder','Utilities',
   function($scope, $timeout, TxBuilder, Utilities) {
@@ -22,14 +22,18 @@ angular.module('bitclip.sendController', [
 
     //TODO: sendPayment Functionality
     $scope.sendPayment = function() {
+      console.log("invoking sendPayment: ", $scope.transactionDetails);
       Utilities.isMainNet().then(function(isMainNet){
         Utilities.getCurrentPrivateKey().then(function(currentPrivateKey){
+          console.log("currentPrivateKey: ", currentPrivateKey);
           TxBuilder.sendTransaction(currentPrivateKey, $scope.transactionDetails, isMainNet).then(function(message){
             $scope.txSuccessMessage = message;
+            $scope.$apply();
             $timeout(function() { $scope.txSuccessMessage = false }, 2000);
           })
           .catch(function(err){
             $scope.txErrorMessage = "Transaction Failed: "+ err.message;
+            $scope.$apply();
             $timeout(function() { $scope.txErrorMessage = false }, 2000);
           });
         });
