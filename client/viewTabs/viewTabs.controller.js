54928eea904f5c32d5769a407353164fde6661f2
JackTanRoo <jack.tan.lu@hotmail.com>
2019-06-30 22:10:46 -0700

    updated code base refactored old apis

--- client/viewTabs/viewTabs.controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('bitclip.viewTabsController', [])
-
-.controller('viewTabsController', ['$scope', function($scope) {
-  $scope.activeTab = 'send';
-  $scope.setActiveTab = function(tab) {
-    $scope.activeTab = tab;
-  };
-}]);
